5e4974a8af23cada3bc6c5eb39b92946b5adb39e
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-02 16:44:02 -0600

    Se creó la navegación de las páginas de registro y autentificación con react router DOM

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function Login() {
   return (
     <>
@@ -46,6 +48,12 @@ export default function Login() {
                 />
             </form>
         </div>
+
+        <nav className="mt-5">
+          <Link to="/auth/registro">
+            ¿No tienes cuenta? Crea una
+          </Link>
+        </nav>
     </>
   )
 }
