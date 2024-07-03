1a70af3bc5eca8af6a07b3e39cb936934681feaf
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-03 09:25:44 -0600

    Se obtiene y se muestra las categorias

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,20 @@
+import { categorias } from "../data/categorias"
+
 export default function Sidebar() {
   return (
-    <div className="w-72">Sidebar</div>
+    <aside className="md:w-72">
+        <div className="p-4">
+            <img 
+                className="w-40"
+                src="img/logo.svg"  
+            />
+        </div>
+
+        <div className="mt-10">
+            {categorias.map( categoria => (
+                <p>{categoria.nombre}</p>
+            ))}
+        </div>
+    </aside>
   )
 }
