fec9b5bccc16c180b1db4f7b4b54102efdc7b340
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-04 12:22:37 -0600

    Se resaltó la categoria actual

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -2,11 +2,13 @@ import useQuiosco from "../hooks/useQuiosco"
 
 export default function Categoria({categoria}) {
 
-  const { handleClickCategoria } = useQuiosco();
+  const { handleClickCategoria, categoriaActual } = useQuiosco();
   const {icono, id, nombre} = categoria
 
+  const resaltarCategoriaActual = () => categoriaActual.id === id ? 'bg-amber-400' : 'bg-white'
+
   return (
-    <div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer">
+    <div className={`${resaltarCategoriaActual()} flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer`}>
         <img 
             src={`/img/icono_${icono}.svg`} 
             alt="Imagen Icono" 
