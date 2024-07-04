a35d277bb6ff07b1056abe48fa19a0feb4bf4114
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-04 10:46:44 -0600

    Se creó un context para manejar el estado de nuestra app

---
+++ src/context/QuioscoProvider.jsx
@@ -0,0 +1,21 @@
+import { createContext } from 'react'
+
+const QuioscoContext = createContext();
+
+const QuioscoProvider = ({children}) => {
+    return (
+        <QuioscoContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </QuioscoContext.Provider>
+    )
+}
+
+export {
+    QuioscoProvider
+}
+
+export default QuioscoContext
