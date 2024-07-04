681c303befd822c98e10b6c7ec8ccd2ed7dc1223
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-04 12:41:32 -0600

    Se filtraron los productos de acuerdo a su categoria

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,4 +1,4 @@
-import { productos } from "../data/productos"
+import { productos as data, productos } from "../data/productos"
 import Producto from "../components/Producto";
 import useQuiosco from "../hooks/useQuiosco";
 
@@ -6,6 +6,8 @@ import useQuiosco from "../hooks/useQuiosco";
 export default function Inicio() {
 
   const { categoriaActual } = useQuiosco()
+
+  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
 
   return (
     <>
