e18ce36b189a13d2d6616a58b2746fa6066f7516
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-05 10:27:37 -0600

    Se muestra el resto de la información del producto

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,4 +1,5 @@
 import useQuiosco from "../hooks/useQuiosco"
+import { formatearDinero } from "../helpers";
 
 export default function ModalProducto() {
 
@@ -21,6 +22,19 @@ export default function ModalProducto() {
 
                     </button>
                 </div>
+                <h1 className="text-3xl font-bold mt-5">
+                    {producto.nombre}
+                </h1>
+                <p className="mt-5 font-black text-5xl text-amber-500">
+                    {formatearDinero(producto.precio)}
+                </p>
+
+                <button
+                    type="button"
+                    className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+                >
+                    Añadir al Pedido
+                </button>
             </div>
         </div>
     )
