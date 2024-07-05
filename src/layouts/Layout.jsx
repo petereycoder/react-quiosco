361f202b5a37cf6d1a0f464481a8102d1f514333
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-05 09:41:41 -0600

    Se configuró y se instaló react-modal

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,29 @@
 import { Outlet } from 'react-router-dom'
+import Modal from 'react-modal'
 import Sidebar from '../components/Sidebar'
 import Resumen from '../components/Resumen'
 import useQuiosco from '../hooks/useQuiosco'
 
 export default function Layout() {
 
-  const { modal } = useQuiosco();
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+
+  const { modal, handleClickModal } = useQuiosco();
   console.log(modal)
 
   return (
-    <div className='md:flex'>
+    <>
+      <div className='md:flex'>
       <Sidebar />
 
       <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
@@ -18,5 +32,17 @@ export default function Layout() {
 
       <Resumen />
     </div>
+
+      <Modal isOpen={modal} style={customStyles} appElement={document.getElementById("root")}>
+        <p>Desde Modal</p>
+        <button
+          onClick={handleClickModal}
+        >
+          Cerrar</button>
+          
+      </Modal>
+    
+
+    </>
   )
 }
