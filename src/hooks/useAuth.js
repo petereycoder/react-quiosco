cb6f016b63c9bf28d71baee6006338c16126e6bc
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-18 09:55:56 -0600

    Se creó un hook para centralizar la auntenticación

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,26 @@
+import clienteAxios from "../config/axios";
+
+export const useAuth = ({middleware, url}) => {
+
+    const login = async (datos, setErrores) => {
+        try {
+            const { data } = await clienteAxios.post('/api/login', datos)
+            localStorage.setItem('AUTH_TOKEN', data.token);
+            setErrores(null);
+        } catch (error) {
+            setErrores(Object.values(error.response.data.errors))
+        }
+    }
+    const registro = () => {
+
+    }
+    const logout = () => {
+
+    }
+
+    return {
+        login,
+        registro,
+        logout
+    }
+}
