e07bdb68c20103f24b170d8ebbf161c202687106
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-18 10:40:00 -0600

    Se registró un useEffect para revalidar el usuario o el error

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,11 @@
+import { useEffect } from 'react'
 import useSWR from "swr";
+import { useNavigate } from 'react-router-dom'
 import clienteAxios from "../config/axios";
 
 export const useAuth = ({middleware, url}) => {
     const token = localStorage.getItem('AUTH_TOKEN')
+    const navigate = useNavigate();
 
     const { data: user, error, mutate } = useSWR('/api/user', () => 
         clienteAxios('/api/user', {
@@ -34,6 +37,13 @@ export const useAuth = ({middleware, url}) => {
     }
     console.log(user)
     console.log(error)
+
+    useEffect(() => {
+        if(middleware === 'guest' && url && user){
+            navigate(url)
+        }
+    }, [user, error])
+
     return {
         login,
         registro,
