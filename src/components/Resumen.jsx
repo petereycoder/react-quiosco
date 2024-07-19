2f8cf5d351789e6f752685906acc67fd5b6160bb
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-19 10:33:23 -0600

    Se envió la petición al endpoint de pedidos

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -4,9 +4,15 @@ import ResumenProducto from "./ResumenProducto"
 
 export default function Resumen() {
 
-  const { pedido, total } = useQuiosco()
+  const { pedido, total, handleSubmitNuevaOrden } = useQuiosco()
 
   const comprobarPedido = () => pedido.length === 0;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    handleSubmitNuevaOrden();
+  }
 
   return (
     <aside className="w-72 h-screen overflow-y-scroll p-5">
@@ -34,7 +40,10 @@ export default function Resumen() {
         Total: {''}
         {formatearDinero(total)}
       </p>
-      <form className="w-full">
+      <form 
+        className="w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="mt-5">
           <input 
             type="submit" 
--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -67,6 +67,23 @@ const QuioscoProvider = ({children}) => {
         setPedido(pedidoActualizado)
         toast.success('Eliminado del Pedido')
     }
+
+    const handleSubmitNuevaOrden = async () => {
+        const token = localStorage.getItem('AUTH_TOKEN')
+        try {
+            await clienteAxios.post('/api/pedidos', 
+            {
+                
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })   
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     return (
         <QuioscoContext.Provider
@@ -82,7 +99,8 @@ const QuioscoProvider = ({children}) => {
                 handleAgregarPedido,
                 handleEditarCantidad,
                 handleEliminarProductoPedido,
-                total
+                total,
+                handleSubmitNuevaOrden
             }}
         >
             {children}
