9cf1deaf7166da8c54a03b368b9ea8b30bbb0639
petereycoder <fp.pipe.reyes@gmail.com>
2024-08-12 17:28:44 -0600

    Se consultan las ordenes

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -1,10 +1,26 @@
+import useSWR from "swr"
+import clienteAxios from "../config/axios"
+
 export default function Ordenes() {
-  return (
-    <div>
-        <h1 className="text-4xl font-black">Ordenes</h1>
-        <p className="text-2xl my-10">
-           Administra las ordenes desde aquí.
-        </p>
-    </div>
-  )
+    const token = localStorage.getItem('AUTH_TOKEN')
+    const fetcher = () => clienteAxios('/api/pedidos', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const {data, error, isLoading} = useSWR('/api/pedidos', fetcher)
+
+    console.log(data)
+    console.log(error)
+    console.log(isLoading)
+
+    return (
+        <div>
+            <h1 className="text-4xl font-black">Ordenes</h1>
+            <p className="text-2xl my-10">
+            Administra las ordenes desde aquí.
+            </p>
+        </div>
+    )
 }
